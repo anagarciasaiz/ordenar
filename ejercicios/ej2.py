54d5ce1800fefa4e2db3d5f4aaeda73e68a51487
Angel <angelmp200@gmail.com>
2023-03-04 22:47:58 +0100

    funcion ordenacion

--- ejercicios/ej2.py
+++ ejercicios/ej2.py
@@ -5,10 +5,24 @@ class ejercicio2():
         for i in range(int(input("Escriba el numero de tareas que va a relizar:"))):
              tarea = 't' + (str(i+1)) #t1 t2 t3....tn
              tareas.append(tarea)
-        random.shuffle(tareas) #metodo que sirve para barajar las tareas 
+        random.shuffle(tareas) #metodo que sirve para barajear las tareas 
         print(tareas)
         orden=[]
         self.tareas=tareas
         self.orden=orden
+    def ordenacion(self,n,t):
+        if len(self.tareas)>0:
+            if 't' + str(n)==self.tareas[t]:
+                print("ha realizado la tarea",n)
+                self.orden.append(self.tareas[t])
+                del self.tareas[t] #eliminamos self.tareas
+                self.ordenacion(n+1,0)
+            else:
+                print("esa tarea no es la q toca,realice la tarea",n)
+        else:
+            print(self.orden)
+
+
+
 
         
