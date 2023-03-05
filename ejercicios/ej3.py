786f0e93267cb620474501b9c955fb51578f55a0
Agirre8 <inagi30@gmail.com>
2023-03-05 20:33:55 +0100

    dividir lista

--- ejercicios/ej3.py
+++ ejercicios/ej3.py
@@ -1,6 +1,22 @@
+import random as rd
+from itertools import islice
+
 
 copia_seguridad=[]
 tabla=[]
+
+def crear_dividir_tabla():
+    n=15
+    for i in range(1,n):
+        tabla.append(rd.randint(0,100))
+
+    divisiones = [2, 4, 3, 6]
+
+    # Usando islice
+    Output = [list(islice(iter(tabla), elem)) for elem in divisiones]
+    
+    print("List after splitting", Output)    
+    return tabla
 
 def esta_explorando():
 
